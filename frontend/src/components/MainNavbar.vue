99540da7266e707618e1b602c099e84de563fbed
Roydevv <roydevgantara472@gmail.com>
2025-07-14 19:56:58 +0700

    responsi

--- frontend/src/components/MainNavbar.vue
+++ frontend/src/components/MainNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-white shadow-md sticky top-0 z-50 animate-fade-in">
     <nav class="container mx-auto px-6 py-4 flex justify-between itemscenter">
-      <div class="text-2xl font-bold text-gray-800">Wahid Nurrohim</div>
+      <div class="text-2xl font-bold text-gray-800">Roy Devgantara Purba</div>
       <ul class="flex space-x-6">
         <li><a href="#profil" class="text-gray-600 hover:text-blue-500 transform hover:-translate-y-0.5 transition-all duration300">Profil</a></li>
         <li><a href="#pendidikan" class="text-gray-600 hover:text-blue-500 transform hover:-translate-y-0.5 transition-all duration300">Pendidikan</a></li>
